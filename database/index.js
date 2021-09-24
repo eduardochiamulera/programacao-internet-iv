f2b4923ed230841d874c0e9cedbf19275adb72cf
Eduardo Antonio Fraga Chiamulera <eduardo.chiamulera@linx.com.br>
2021-09-23 22:22:11 -0300

    senha do banco removida para .env

--- database/index.js
+++ database/index.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 // Ligar á B.D.: 'test'->user da BD, ´nnn´->pass
 //mongoose.connect('mongodb+srv://clZISyhI02sq9QPI:FiON9kBJAIQ4cL25@cluster0.gprnp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
-mongoose.connect('mongodb://localhost:27017/produtos?readPreference=primary&appname=MongoDB%20Compass&ssl=false');
-
+console.log(process.env.DBSECRET)
+mongoose.connect(`mongodb://produtosApplication:${process.env.DBSECRET}@localhost:27017/produtos?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false`);
 // Confirma ligação na consola
 mongoose.connection.on('connected', function () {
   console.log('Connected to Database '+'test');
